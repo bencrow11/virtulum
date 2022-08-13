3442a13aade332e8ab9b092a2732c57b2e836c01
Ben <bencrow11@gmail.com>
2022-08-13 17:45:55 +0100

    Added lib folder and licence

---
+++ lib/interfaces.ts
@@ -0,0 +1,49 @@
+export interface Item {
+  quantity: number;
+  description: string;
+  unitPrice: number;
+  amount: number;
+}
+
+export interface Invoice {
+  company: Company;
+  items: Item[];
+  invoiceNumber: string;
+  invoiceDate: Date;
+  dueDate: Date;
+  subtotal: number;
+  tax: number;
+  total: number;
+  isPaid: boolean;
+}
+
+export interface Address {
+  address: string;
+  city: string;
+  postcode: string;
+}
+
+export interface Company {
+  name: string;
+  contact: string;
+  bill: Address;
+  ship: Address;
+  invoices: Invoice[];
+  invoiceNumber: number;
+}
+
+export interface Account {
+  credentials: {
+    email: string;
+    password: string;
+  };
+  details: {
+    name: string;
+    company: string;
+    address: Address;
+    number?: number;
+    email?: string;
+    website?: string;
+  };
+  companies: Company[];
+}
